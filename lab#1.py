6291fcb88d7d2ac433c60b62992257d2600359dd
Muddassar Ali <81901565+muddassar412@users.noreply.github.com>
2025-04-14 14:01:35 +0500

    Update lab#1.py

--- lab#1.py
+++ lab#1.py
@@ -1,1 +1,11 @@
-
+import sklearn
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+x = np.array([[5],[15],[25],[35],[45],[55]])#.reshape((-1,1))
+y = np.array([5,20,14,32,22,38])
+modle = linear_model.LinearRegression()
+modle.fit(x,y)
+x_new = np.array([150]).reshape((-1,1))
+y-new = modle.predict(x_new)
+print(y-new)
