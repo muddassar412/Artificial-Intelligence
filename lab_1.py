059bcc3e9e079e5e199522902457fd370b8ed152
Muddassar Ali <muddassar.developer@gmail.com>
2025-04-14 17:39:09 +0500

    commit

--- lab_1.py
+++ lab_1.py
@@ -7,8 +7,8 @@ y = np.array([5,20,14,32,22,38])
 modle = linear_model.LinearRegression()
 modle.fit(x,y)
 x_new = np.array([150]).reshape((-1,1))
-y-new = modle.predict(x_new)
-print(y-new)
+y_new = modle.predict(x_new)
+print(y_new) #output
 
 
 """
